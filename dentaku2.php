d199d0b3cd76dd04f62589a19efae8d181271461
初コミット <sawanoyuuta@sawanoyuuta-no-MacBook-Pro.local>
2016-01-28 18:07:27 +0900

    セカンドコミット

--- dentaku2.php
+++ dentaku2.php
@@ -1,5 +1,5 @@
 
-
+<!DOCTYPE html>
 
 <html>
   <head>
@@ -7,7 +7,7 @@
   </head>
   <body>
 
-  <form name="form1" action="dentaku2.php" method="post">
+    <form name="form1" action="dentaku2.php" method="post">
       <input type = "text" name = "txtA">　
 
       <select name="selOpe" size=1>
@@ -18,61 +18,54 @@
       </select>　
 
       <input type = "text" name = "txtB">　=　?　
-      
+
       <br>
       <input type = "submit" value = "計算">
       <input type = "reset" value = "クリア">
 
     </form>
-    
+
     <?php
     //var_dump(isset($_POST['txtA'], $_POST['txtB'], $_POST['selOpe']));
     //var_dump(is_numeric($_POST['txtA']));
-      function e($str, $charset = 'UTF-8'){
-        return htmlspecialchars($str, ENT_QUOTES, $charset);
+    
+    if (isset($_POST['txtA'], $_POST['txtB'], $_POST['selOpe']) && is_numeric($_POST['txtA']) && is_numeric($_POST['txtB']) && ($_POST['txtB'])!= 0){
+
+
+      $a = e($_POST['txtA']);
+      $b = e($_POST['txtB']);
+      $ope = e($_POST['selOpe']);
+
+      switch ($ope) {
+        case "＋":
+          $answer = $a + $b;
+          break;
+        case "－":
+          $answer = $a - $b;
+          break;
+        case "×":
+          $answer = $a * $b;
+          break;
+        case "÷":
+          $answer = $a / $b;
+          break;
+        default:
+          break;
       }
 
+      print ($a . " " . $ope . " " . $b . " = " . $answer . "\n");
 
-      if (isset($_POST['txtA'], $_POST['txtB'], $_POST['selOpe']) && is_numeric($_POST['txtA']) && is_numeric($_POST['txtB'])) {
-        
-        
+    }else{
+      print '不正';
+    }
 
+    function e($str, $charset = 'UTF-8'){
+      return htmlspecialchars($str, ENT_QUOTES, $charset);
+    }
 
-          $a = e($_POST['txtA']);
-          $b = e($_POST['txtB']);
-          $ope = e($_POST['selOpe']);
-
-          
-
-              switch ($ope) {
-                case "＋":
-                  $answer = $a + $b;
-                  break;
-                case "－":
-                  $answer = $a - $b;
-                  break;
-                case "×":
-                  $answer = $a * $b;
-                  break;
-                case "÷":
-                  $answer = $a / $b;
-                  break;
-                default:
-                  break;
-              }
-
-              
-              print ($a." ".$ope." ".$b." = ".$answer."\n");
-            
-
-        }else{
-          print '入力した文字は不正';
-          }
-      
-
-        ?>
+    ?>
     <br />
     <br />
-    
+
   </body>
 </html>
