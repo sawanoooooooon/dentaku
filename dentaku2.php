35094b57378a4c7a6c758cb56d06313d2ac3617a
初コミット <sawanoyuuta@sawanoyuuta-no-MacBook-Pro.local>
2016-01-28 19:13:19 +0900

    サードコミット

--- dentaku2.php
+++ dentaku2.php
@@ -8,16 +8,16 @@
   <body>
 
     <form name="form1" action="dentaku2.php" method="post">
-      <input type = "text" name = "txtA">　
+      <input type = "text" name = "txtA">
 
       <select name="selOpe" size=1>
         <option value = "＋">＋</option>
         <option value = "－">－</option>
         <option value = "×">×</option>
         <option value = "÷">÷</option>
-      </select>　
+      </select>
 
-      <input type = "text" name = "txtB">　=　?　
+      <input type = "text" name = "txtB">  =  ?
 
       <br>
       <input type = "submit" value = "計算">
@@ -28,13 +28,28 @@
     <?php
     //var_dump(isset($_POST['txtA'], $_POST['txtB'], $_POST['selOpe']));
     //var_dump(is_numeric($_POST['txtA']));
-    
-    if (isset($_POST['txtA'], $_POST['txtB'], $_POST['selOpe']) && is_numeric($_POST['txtA']) && is_numeric($_POST['txtB']) && ($_POST['txtB'])!= 0){
+
+    $err_msg = "";
 
 
-      $a = e($_POST['txtA']);
-      $b = e($_POST['txtB']);
-      $ope = e($_POST['selOpe']);
+    //if (isset($_POST['txtA'], $_POST['txtB'], $_POST['selOpe']) && is_numeric($_POST['txtA']) && is_numeric($_POST['txtB']) && ($_POST['txtB'])!= 0){
+    if (!isset($_POST['txtA'], $_POST['txtB'], $_POST['selOpe'])){
+      $err_msg = '文字を入力してください';
+
+    } elseif (!is_numeric($_POST['txtA']) && !is_numeric($_POST['txtB'])){
+      $err_msg ='数値を入力してください';
+
+    } elseif (($_POST['selOpe']) == "÷" && ($_POST['txtB']) == 0){
+      $err_msg ='0で割れないです';
+    }
+
+    if(!empty($err_msg)){
+      print e("$err_msg");
+
+    }else{
+      $a = $_POST['txtA'];
+      $b = $_POST['txtB'];
+      $ope = $_POST['selOpe'];
 
       switch ($ope) {
         case "＋":
@@ -53,10 +68,7 @@
           break;
       }
 
-      print ($a . " " . $ope . " " . $b . " = " . $answer . "\n");
-
-    }else{
-      print '不正';
+      print e($a . " " . $ope . " " . $b . " = " . $answer . "\n");
     }
 
     function e($str, $charset = 'UTF-8'){
