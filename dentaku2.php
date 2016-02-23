da961ed6464639723866aa60af9028223712c21f
初コミット <sawanoyuuta@sawanoyuuta-no-MacBook-Pro.local>
2016-02-23 17:59:15 +0900

    ７コミット　      $txta = mb_convert_kana($_POST['txtA'],"n");      $txtb = mb_convert_kana($_POST['txtB'],"n"); の場所を変更

--- dentaku2.php
+++ dentaku2.php
@@ -30,39 +30,42 @@
     if (isset($_POST['Submit'])){
       $err_msg = "";
       $answer = "";
-      $ope =$_POST['selOpe'];
 
-      if (!isset($txta, $txtb, $ope)){
+
+      if (!isset($_POST['txtA'], $_POST['txtB'], $_POST['selOpe'])){
         $err_msg = '文字を入力してください';
-
-      } elseif (!is_numeric($txta) || !is_numeric($txtb)){
+      } elseif (!is_numeric(mb_convert_kana($_POST['txtA'],"n")) || !is_numeric(mb_convert_kana($_POST['txtB'],"n"))){
         $err_msg ='数値を入力してください';
+      } else {
+        $txta = mb_convert_kana($_POST['txtA'],"n");
+        $txtb = mb_convert_kana($_POST['txtB'],"n");
+        $ope = $_POST['selOpe'];
+        
+        switch ($ope) {
+          case "＋":
+            $answer = $txta + $txtb;
+            break;
+          case "－":
+            $answer = $txta - $txtb;
+            break;
+          case "×":
+            $answer = $txta * $txtb;
+            break;
+          case "÷":
+            if ($txtb == 0){
+              $err_msg = '0で割れないです';
+              break;
+              }
+            $answer = $txta / $txtb;
+            break;
+          default:
+            $err_msg ='計算できません';
+            break;
+        }
       }
 
-      $txta = mb_convert_kana($_POST['txtA'],"n");
-      $txtb = mb_convert_kana($_POST['txtB'],"n");
 
-      switch ($ope) {
-        case "＋":
-          $answer = $txta + $txtb;
-          break;
-        case "－":
-          $answer = $txta - $txtb;
-          break;
-        case "×":
-          $answer = $txta * $txtb;
-          break;
-        case "÷":
-          if ($txtb == 0){
-            $err_msg = '0で割れないです';
-            break;
-            }
-          $answer = $txta / $txtb;
-          break;
-        default:
-          $err_msg ='計算できません';
-          break;
-        }
+     
 
       if(!empty($err_msg)){
         print "$err_msg";
